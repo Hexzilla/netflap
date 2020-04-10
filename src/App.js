170f15e253b296f8e4fe1dfe34c71d01ac0b4b28
reactomoss <reactomoss@gmail.com>
2020-04-10 14:13:11 +0300

    update homepage with footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Accordion, Card, Header, Feature, Form, Wrapper } from './components';
+import {
+  Accordion,
+  Card,
+  Header,
+  Feature,
+  Form,
+  Footer,
+  Wrapper,
+} from './components';
 import './App.css';
 import logo from './logo.svg';
 
@@ -130,6 +138,43 @@ export function App() {
           </Form.Text>
         </Form>
       </Accordion>
+
+      <Footer>
+        <Footer.Title>Questions? Contact us.</Footer.Title>
+        <Footer.Break />
+        <Footer.Row>
+          <Footer.Column>
+            <Footer.Link href="#">FAQ</Footer.Link>
+            <Footer.Link href="#">Investor Relations</Footer.Link>
+            <Footer.Link href="#">Ways to Watch</Footer.Link>
+            <Footer.Link href="#">Corporate Information</Footer.Link>
+            <Footer.Link href="#">Netflix Originals</Footer.Link>
+          </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href="#">Help Centre</Footer.Link>
+            <Footer.Link href="#">Jobs</Footer.Link>
+            <Footer.Link href="#">Terms of Use</Footer.Link>
+            <Footer.Link href="#">Contact Us</Footer.Link>
+          </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href="#">Account</Footer.Link>
+            <Footer.Link href="#">Redeem gift cards</Footer.Link>
+            <Footer.Link href="#">Privacy</Footer.Link>
+            <Footer.Link href="#">Speed Test</Footer.Link>
+          </Footer.Column>
+
+          <Footer.Column>
+            <Footer.Link href="#">Media Centre</Footer.Link>
+            <Footer.Link href="#">Buy gift cards</Footer.Link>
+            <Footer.Link href="#">Cookie Preferences</Footer.Link>
+            <Footer.Link href="#">Legal Notices</Footer.Link>
+          </Footer.Column>
+        </Footer.Row>
+        <Footer.Break />
+        <Footer.Text>Netflix United Kingdom</Footer.Text>
+      </Footer>
     </>
   );
 }
